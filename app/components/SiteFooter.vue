ab58006d35f1255f1cb2c538cda407de173873ca
joseph-cou <joseph-cou@users.noreply.github.com>
2026-02-16 17:22:49 +0000

    layout, components, and mesh gradient background

---
+++ app/components/SiteFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="sm:mt-32 flex-none">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="pb-16 pt-10">
+        <div
+          class="flex flex-col items-center justify-between gap-2 md:flex-row"
+        >
+          <p class="text-sm text-white/60">
+            &copy; {{ new Date().getFullYear() }} First Principles Studio Ltd.
+            All rights reserved.
+          </p>
+          <p class="text-sm text-white/60">Company number 16962497</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ app/components/SiteHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav
+      class="mx-auto max-w-7xl flex items-center px-6 py-8 lg:px-8 lg:py-12"
+      aria-label="Global"
+    >
+      <div class="flex lg:flex-1">
+        <NuxtLink to="/" class="-m-1.5 p-1.5 group flex items-center gap-3">
+          <img
+            src="/fp-logo.png"
+            alt="first principles logo"
+            class="h-10 w-auto"
+          />
+          <span
+            class="text-2xl font-extrabold tracking-tight text-white transition group-hover:text-teal-400"
+          >
+            first principles
+          </span>
+        </NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
